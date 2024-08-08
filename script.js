96cc9585acd1335a54882e725ef16df3e5adc1ee
jabin <jabin@bititude.com>
2024-08-08 16:46:03 +0530

    Corrected playRound function to properly handle game logic and output

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Variables
-const userChoice = prompt("Enter your Choice..!")
+let userChoice = prompt("Enter your Choice..!");
 let computerChoice;
 let humanScore = 0;
 let computerScore = 0;
 let result;
+
 
 // Computer Choice 
 
@@ -11,31 +12,33 @@ function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
-            return "Bot Choice :" + " " + "rock";
+            return "rock";
         case 1:
-            return "Bot Choice :" + " " + "paper";
+            return "paper";
         case 2:
-            return "Bot Choice :" + " " + "scissors";
+            return "scissors";
     }
 }
-console.log(getComputerChoice());
+console.log("Bot Choice :" + " " + getComputerChoice());
 
 
 // User Choice
 
 function getHumanChoice() {
-    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
-        return "Your Choice : " + ' ' + userChoice;
+    userChoice = userChoice.toLowerCase();
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userChoice;
     } else {
         return ("Invalid Choice! Enter 'rock','paper' or 'scissors'.")
     }
 }
-console.log(getHumanChoice());
+console.log("Your Choice : " + ' ' + getHumanChoice());
 
 // Single Round
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
+
+    if (humanChoice === computerChoice) {
         result = "Its's a Tie.";
     } else {
         switch (humanChoice) {
@@ -49,20 +52,13 @@ function playRound(humanChoice, computerChoice) {
                 result = (computerChoice === "paper") ? "You Win! Scissors Beats Paper" : "You Lose, Bot Get the Point";
                 break;
             default:
-                result = "Invalid Choice"
+                result = "Invalid Choice";
         }
     }
+    console.log(result);
 }
-console.log(result);
-
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
-
-// play Game 
-
-function playGame(playRound) {
-
-}
