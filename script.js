7795f08c6bb78d9b3ebac89dfccaf649647004f0
jabin <jabin@bititude.com>
2024-08-23 12:46:00 +0530

    Update Js File using DOM

--- script.js
+++ script.js
@@ -1,103 +1,97 @@
-// variables
 
-let userChoice;
-let computerChoice;
-let humanScore = 0;
-let computerScore = 0;
-let result;
+document.addEventListener("DOMContentLoaded", () => {
+    // variables
 
-// User Choice
+    let computerChoice;
+    let humanScore = 0;
+    let computerScore = 0;
+    let result = "";
 
-function getHumanChoice(userChoice) {
-    let choice = userChoice.toLowerCase()
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return choice;
-    } else {
-        return ("Invalid Choice! Enter 'rock','paper' or 'scissors'.")
-    }
-}
+    // Computer Choice
 
-// Computer Choice
-
-function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3);
-    switch (computerChoice) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-    }
-}
-
-
-// // Single Round
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        result = "Its's a Tie.";
-    } else {
-        switch (humanChoice) {
-            case "rock":
-                if (computerChoice == "scissors") {
-                    humanScore += 1;
-                    result = "You Win! Paper Beats Rock"
-                } else {
-                    computerScore += 1;
-                    result = "You Lose, Bot Get the Point";
-                }
-                break;
-            case "paper":
-                if (computerChoice === "rock") {
-                    humanScore += 1;
-                    result = "You Win! Paper Beats Rock"
-                } else {
-                    computerScore += 1;
-                    result = "You Lose, Bot Get the Point";
-                }
-                break;
-            case "scissors":
-                if (computerChoice === "paper") {
-                    humanScore += 1;
-                    result = "You Win! Paper Beats Rock"
-                } else {
-                    computerScore += 1;
-                    result = "You Lose, Bot Get the Point";
-                }
-                break;
-            default:
-                result = "Invalid Choice";
+    function getComputerChoice() {
+        computerChoice = Math.floor(Math.random() * 3);
+        switch (computerChoice) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2:
+                return "scissors";
         }
     }
-    return result;
-}
 
-function playGame() {
+    // Game Logic
 
-    for (i = 0; i < 5; i++) {
-        let userChoice = prompt("Enter your Choice..!");
-        humanSelection = getHumanChoice(userChoice);
-        computerSelection = getComputerChoice();
-        console.log("Your Choice : " + ' ' + humanSelection);
-        console.log("Bot Choice :" + " " + computerSelection);
+    function playRound(humanChoice) {
 
+        computerChoice = getComputerChoice();
+        if (humanChoice === computerChoice) {
+            result = "Its's a Tie.";
+        } else {
+            switch (humanChoice) {
+                case "rock":
+                    if (computerChoice === "scissors") {
+                        humanScore += 1;
+                        result = "You Win! Paper Beats Rock"
+                    } else {
+                        computerScore += 1;
+                        result = "You Lose, Bot Get the Point";
+                    }
+                    break;
+                case "paper":
+                    if (computerChoice === "rock") {
+                        humanScore += 1;
+                        result = "You Win! Paper Beats Rock"
+                    } else {
+                        computerScore += 1;
+                        result = "You Lose, Bot Get the Point";
+                    }
+                    break;
+                case "scissors":
+                    if (computerChoice === "paper") {
+                        humanScore += 1;
+                        result = "You Win! Paper Beats Rock"
+                    } else {
+                        computerScore += 1;
+                        result = "You Lose, Bot Get the Point";
+                    }
+                    break;
+            }
+        }
 
-        result = playRound(humanSelection, computerSelection)
-        console.log(result);
+        document.querySelector('.outcome').textContent = `OutCome :- ${result}`;
+        document.getElementById('humanScore').textContent = humanScore;
+        document.getElementById('computerScore').textContent = computerScore;
 
-        console.log(`Round ${i + 1}: -`);
-        console.log(`Your Score ${humanScore} `);
-        console.log(`Bot ${computerScore} `);
-
+        if (humanScore === 5) {
+            document.querySelector('.outcome').textContent = "Congratulation! You Win The Game.";
+        } else if (computerScore === 5) {
+            document.querySelector('.outcome').textContent = "Better Luck next Time! Bot Win The Game.";
+        }
+        stop()
     }
-    if (humanScore > computerScore) {
-        console.log("Congratulation! You Win The Game.")
-    } else if (humanScore < computerScore) {
-        console.log("Better Luck next Time! Bot Win The Game.");
-    } else {
-        console.log("Wooh! Its a tie.");
+    // return result;
+
+    function stop() {
+        if (humanScore === 5 || computerScore === 5) {
+            document.querySelector("button").disabled = true;
+        }
     }
 
-}
-console.log(playGame());
+    function reset() {
+        humanScore = 0;
+        computerScore = 0;
+        document.getElementById('humanScore').textContent = humanScore;
+        document.getElementById('computerScore').textContent = computerScore;
+        document.querySelector('.outcome').textContent = "Choose your move.";
+    }
+
+    //  Changing into DOM 
+
+    document.getElementById('rock').addEventListener('click', () => playRound('rock'));
+    document.getElementById('paper').addEventListener('click', () => playRound('paper'));
+    document.getElementById('scissors').addEventListener('click', () => playRound('scissors'));
+    document.querySelector('.reset').addEventListener('click', reset);
+
+});
